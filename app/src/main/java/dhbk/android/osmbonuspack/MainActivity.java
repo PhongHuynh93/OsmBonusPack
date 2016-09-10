46a08f665ba08e3144cabcb0a39089f0ea11fc06
Phong <p7157568@gmail.com>
2016-09-10 15:31:34 +0700

    feat - add marker + infowindow

--- app/src/main/java/dhbk/android/osmbonuspack/MainActivity.java
+++ app/src/main/java/dhbk/android/osmbonuspack/MainActivity.java
@@ -1,7 +1,13 @@
 package dhbk.android.osmbonuspack;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        init map
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        GeoPoint startPoint = new GeoPoint(48.13, -1.63);
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        mapController.setCenter(startPoint);
+
+//        init marker
+        NameOfStreetOverlay startMarker = new NameOfStreetOverlay(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+
+        // add marker overlay
+        map.getOverlays().add(startMarker);
+
+        // make the instruction table
+//        startMarker.setIcon(getResources().getDrawable(R.mipmap.ic_launcher));
+        startMarker.setTitle("Start point");
+
+        // add name of street on map
+        addCustomOverlay();
+    }
+
+    private void addCustomOverlay() {
+
     }
 }
---
+++ app/src/main/java/dhbk/android/osmbonuspack/NameOfStreetInfoWindow.java
@@ -0,0 +1,42 @@
+package dhbk.android.osmbonuspack;
+
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.infowindow.BasicInfoWindow;
+
+/**
+ * Created by huynhducthanhphong on 9/10/16.
+ */
+public class NameOfStreetInfoWindow extends BasicInfoWindowPhong {
+    protected NameOfStreetOverlay mMarkerRef;
+
+    public NameOfStreetInfoWindow(int layoutResId, MapView mapView) {
+        super(layoutResId, mapView);
+    }
+
+    public NameOfStreetOverlay getMarkerReference() {
+        return this.mMarkerRef;
+    }
+
+    public void onOpen(Object item) {
+        super.onOpen(item);
+        this.mMarkerRef = (NameOfStreetOverlay) item;
+        ImageView imageView = (ImageView) this.mView.findViewById(mImageId);
+        Drawable image = this.mMarkerRef.getImage();
+        if (image != null) {
+            imageView.setImageDrawable(image);
+            imageView.setVisibility(0);
+        } else {
+            imageView.setVisibility(8);
+        }
+
+    }
+
+    public void onClose() {
+        super.onClose();
+        this.mMarkerRef = null;
+    }
+}
